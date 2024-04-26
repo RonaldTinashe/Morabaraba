222f5925f8b65842fa0bfb1b213abf9ccdb3f564
Ronald Munodawafa <ronaldmunodawafa@gmail.com>
2024-04-26 12:58:10 +0000

    Move binary tree implementation into its own module

--- Morabaraba/Program.fs
+++ Morabaraba/Program.fs
@@ -1,27 +1,5 @@
 ﻿module Morabaraba
 
-[<RequireQualifiedAccess>]
-module BinaryTree =
-    type Node<'T> =
-        | Node of value: 'T * left: Node<'T> * right: Node<'T>
-        | NoValue
-
-    let rec fold folder shouldSelectLeft state tree =
-        match tree with
-        | NoValue -> state
-        | Node(value, left, right) ->
-            let state2 = folder state value
-
-            if shouldSelectLeft state2 then
-                let state3 = fold folder shouldSelectLeft state2 left
-                state3
-            else
-                match right with
-                | NoValue -> state2
-                | Node _ ->
-                    let state4 = fold folder shouldSelectLeft state right
-                    state4
-
 type Shade =
     | Dark
     | Light
