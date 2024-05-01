2623073fb04c039de16f9d79d0e82cd9ef2f44c6
Ronald Munodawafa <ronaldmunodawafa@gmail.com>
2024-05-01 05:59:59 +0000

    Reformat for brevity

--- Morabaraba/BinaryTree.fs
+++ Morabaraba/BinaryTree.fs
@@ -5,18 +5,18 @@ type Node<'T> =
     | Node of value: 'T * left: Node<'T> * right: Node<'T>
     | Empty
 
-let rec fold folder shouldSelectLeft state tree =
+let rec fold folder leftBranchDiscriminant state tree =
     match tree with
     | Empty -> state
     | Node(value, left, right) ->
         let state2 = folder state value
 
-        if shouldSelectLeft state2 then
-            let state3 = fold folder shouldSelectLeft state2 left
+        if leftBranchDiscriminant state2 then
+            let state3 = fold folder leftBranchDiscriminant state2 left
             state3
         else
             match right with
             | Empty -> state2
             | Node _ ->
-                let state4 = fold folder shouldSelectLeft state right
+                let state4 = fold folder leftBranchDiscriminant state right
                 state4
